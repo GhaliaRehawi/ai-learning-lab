5c9233f1c4b991b39cad4a4a4f292d904f8f1e28
GhaliaRehawi <ghalia.rihawi93@gmail.com>
2026-06-09 13:09:54 +0200

    EDIT: add comments to gene_means.py, add new dev dependencies to pyproject.toml

--- 01-python-foundation/exercises/gene_means.py
+++ 01-python-foundation/exercises/gene_means.py
@@ -7,9 +7,9 @@ def gene_means(path: str) -> dict[str, float]:
     """compute per-gene mean expression across samples"""
     with open(path, "r") as f:
         reader = csv.DictReader(f)
-        data = defaultdict(list)
+        data = defaultdict(list) # auto-initializes any new key to []
         for row in reader:
-            data[row["gene"]].append(float(row["expression"]))
+            data[row["gene"]].append(float(row["expression"])) # shape {gene1: [exp1,exp2,,]}
 
     return {gene: mean(values) for gene, values in data.items()}
 
